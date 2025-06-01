822a95083d59f8aaa428890899f4dca75acb4fec
christopherbritton <36803008+christopherbritton@users.noreply.github.com>
2025-06-01 06:09:33 +0000

    Implemented unit tests for each routine for each class; updated CMakeList to integrate unit tests; included .txt files of console output for simulation and unit tests.

---
+++ build_tests/CMakeFiles/evtol_sim.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/EVTOL_Abstraction/src/AlphaEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/AlphaEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/BravoEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/BravoEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/CharlieEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/CharlieEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/DeltaEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/DeltaEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/EchoEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/EchoEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/FleetManager.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/FleetManager.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/main.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/evtol_sim.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_tests/CMakeFiles/test_evtol_methods.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/EVTOL_Abstraction/src/AlphaEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/AlphaEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/BravoEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/BravoEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/CharlieEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/CharlieEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/DeltaEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/DeltaEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/EchoEVTOL.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/EchoEVTOL.cpp.o"
+  "/workspaces/EVTOL_Abstraction/src/FleetManager.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/src/FleetManager.cpp.o"
+  "/workspaces/EVTOL_Abstraction/tests/test_evtol_methods.cpp" "/workspaces/EVTOL_Abstraction/build_tests/CMakeFiles/test_evtol_methods.dir/tests/test_evtol_methods.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
